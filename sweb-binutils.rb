331cf54d4bed3a4b638016f69ea5049e3637e714
Alexander Skiba <mail@ghostlyrics.net>
2015-10-03 01:57:02 +0200

    update binutils to 2.25.1

--- sweb-binutils.rb
+++ sweb-binutils.rb
@@ -1,16 +1,15 @@
-require 'formula'
-
 class SwebBinutils < Formula
-  homepage 'http://www.gnu.org/software/binutils/binutils.html'
-  url 'http://ftpmirror.gnu.org/binutils/binutils-2.24.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz'
-  sha1 '1b2bc33003f4997d38fadaa276c1f0321329ec56'
+  desc "FSF Binutils for native development"
+  homepage "https://www.gnu.org/software/binutils/binutils.html"
+  url "http://ftpmirror.gnu.org/binutils/binutils-2.25.1.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/binutils/binutils-2.25.1.tar.gz"
+  sha256 "82a40a37b13a12facb36ac7e87846475a1d80f2e63467b1b8d63ec8b6a2b63fc"
 
   bottle do
-    root_url "https://icg.tugraz.at/~skiba/homebrew"
-    revision 2
-    sha256 "c6ad597609efad398b92017c80bd3df530d60cf283502a725575b3acb680037a" => :yosemite
-    sha256 "ec25f1a5e7d7f3b51a62930ecde198a59538c1a4bc950ee613751b3cd36726a8" => :el_capitan
+    # root_url "https://icg.tugraz.at/~skiba/homebrew"
+    # revision 2
+    # sha256 "c6ad597609efad398b92017c80bd3df530d60cf283502a725575b3acb680037a" => :yosemite
+    # sha256 "ec25f1a5e7d7f3b51a62930ecde198a59538c1a4bc950ee613751b3cd36726a8" => :el_capitan
   end
 
   def arch
@@ -56,7 +55,9 @@ class SwebBinutils < Formula
   end
 
   test do
-    assert `#{bin}/gnm #{bin}/gnm`.include? 'main'
-    assert_equal 0, $?.exitstatus
+    assert_match /main/, shell_output("#{bin}/gnm #{bin}/gnm")
+
+    # assert `#{bin}/gnm #{bin}/gnm`.include? 'main'
+    # assert_equal 0, $?.exitstatus
   end
 end
